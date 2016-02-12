56c195f8972e8cffc33bf3d34394d97c53255ccc
Mats Jonassen <mats.jonassen@lyse.net>
2016-02-12 16:56:52 +0100

    Added func to find lines in log

--- RefactorResults/RefactorResults/MatsTesting.py
+++ RefactorResults/RefactorResults/MatsTesting.py
@@ -4,35 +4,34 @@ import getopt
 import HTMLParser
 import os
 import xml.etree.ElementTree as ET
-print "Command line worked"
-"""
-opts, args = getopt.getopt(sys.argv[1:], "i:o:", ["inputFile=", "outputLocation="])
-for opt, arg in opts:
-    if opt in ("-i", "--inputFile"):
-        inputfile = arg
-    if opt in ("-o", "--outputLocation"):
-        outputLocation = arg
-print inputfile
-print outputLocation"""
 
-#class SubTest(object):
-
-"""
-class HTMLParser(HTMLParser.HTMLParser):
-    def handle_data(self, data):
-        print "Encountered data: %s" % data;
+path = ""
+path = "D:/Dropbox/Bachelor2016/In/20dc7a85-9275-4f2d-b84d-7c3568f0e9a9/NO-W-LMXMTH3/LoadNewSwUnitTestrack.xml.SwUpgradeTest Acu.txt"
+#file = open(path, 'r+')
+inStamp = "2015-11-17 05:17:28,717"
 
 
-print os.getcwd()
-file = open("test.html", "r")
-parser = HTMLParser()
-while True:
-    parser.feed(file.readline())
-    raw_input()
-"""
+def locateLinesInLog(filePath, timeStamp, preLines, postLines):
+    lines = []
+    file = open(filePath, "r+")
+    stamp_found = False
+    for i in range(preLines):
+        lines.append("")
+    while not stamp_found:
+        line = file.readline()
+        if line.startswith(timeStamp):
+            stamp_found = True
+            lines.append(line)
+        else:
+            for i in range(preLines):
+                if i != 2:
+                    lines[i] = lines[i+1]
+                else:
+                    lines[2] = line
+    for i in range(postLines):
+        lines.append(file.readline())
+    file.close()
+    return lines
 
-file = open("IciIpsTest.trx", "r")
-root = ET.fromstring(file.read())
-subtests = root.find("InnerTests")
-for child in subtests:
-    print child.find("TestResult").text
+arr = locateLinesInLog(path, inStamp, 3, 3)
+print arr
