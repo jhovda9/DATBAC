c4811f1740018427b65cfdb04b0e1bd5486d8796
Mats Jonassen <mats.jonassen@lyse.net>
2016-02-24 15:08:24 +0100

    not sure

--- RefactorResults/RefactorResults/EntryPoint.py
+++ RefactorResults/RefactorResults/EntryPoint.py
@@ -1,12 +1,14 @@
 import RefactorResults as RR
 import os
-#path = "D:Dropbox/SharedBachelor2016/TestData/xm3"
-path = "C:/Users/matsj/Source/Repos/DATBAC/RefactorResults/RefactorResults/"
+path = "D:Dropbox/SharedBachelor2016/TestData/xm3"
+#path = "C:/Users/matsj/Source/Repos/DATBAC/RefactorResults/RefactorResults/"
 #path = "C:Users/matsj/Source/Repos/DATBAC/RefactorResults/RefactorResults/HVC02.txt"
+
+RR.generateTestReport(path)
 
 """
 path = "D:Dropbox/SharedBachelor2016/TestData/xm3/Mcob02Ccpu.txt"
 inStamp = "2016-02-22 10:36:21,765"
 print RR.locateLinesInLog(path,inStamp,3,3)
-"""
 
+"""
